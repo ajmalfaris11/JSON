3d9e9ee607092ec31e19a4dc73a112d63476fccf
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-07-28 21:13:16 +0530

    fetch the data from studentsData.json file

---
+++ students/script.js
@@ -0,0 +1,15 @@
+
+// define loadData
+function loadData(){
+    // the fetch is using to bring data from another location through fetch
+    // inside teh brackets we give the location where we are going to get datas
+    // it might be from inside or outside
+    
+    fetch("./studentData.json")
+    // fetch is a netword request
+    // after fetching we get a response
+    .then(response => response.json()) //only taking json file from response
+    .then(data => { // and that response store to data (json file) 
+        console.log(data);
+    })
+}
